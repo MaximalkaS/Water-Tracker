48bdc9153ff077c117d45c95896b1da7f58e5fe0
MaximalkaS <rodgerr13150@gmail.com>
2026-04-09 15:10:02 +0300

    feat: add WaterOptionCardModel and mapper for UI

---
+++ Water Tracker/Core/Mapper/WaterOptionMapper.swift
@@ -0,0 +1,8 @@
+//
+//  WaterOptionMapper.swift
+//  Water Tracker
+//
+//  Created by Maximka on 09.04.2026.
+//
+
+import Foundation
---
+++ Water Tracker/Core/Models/WaterOptionCardModel.swift
@@ -0,0 +1,8 @@
+//
+//  WaterOptionCardModel.swift
+//  Water Tracker
+//
+//  Created by Maximka on 09.04.2026.
+//
+
+import Foundation
