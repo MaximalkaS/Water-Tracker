655bc0e0e2b88f31c902c3f8e6b10a6a3f46bb41
MaximalkaS <rodgerr13150@gmail.com>
2026-04-11 14:18:53 +0300

    feat: add daily date and persist selected water portion

---
+++ Water Tracker/Core/Extensions/DateFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Formatter.swift
+//  Water Tracker
+//
+//  Created by Maximka on 11.04.2026.
+//
+
+import Foundation
---
+++ Water Tracker/Core/Extensions/UINavigationController.ext.swift
@@ -0,0 +1,8 @@
+//
+//  UINavigationController.ext.swift
+//  Water Tracker
+//
+//  Created by Maximka on 11.04.2026.
+//
+
+import Foundation
